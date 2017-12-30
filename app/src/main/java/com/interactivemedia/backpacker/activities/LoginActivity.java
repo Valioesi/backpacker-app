fafc953f40043b65ed0054dae8d0ea6eda8bda38
Valioesi <Vali_Schagerl@web.de>
2017-12-30 19:24:22 +0100

    Optimized imports in LoginActivity

--- app/src/main/java/com/interactivemedia/backpacker/activities/LoginActivity.java
+++ app/src/main/java/com/interactivemedia/backpacker/activities/LoginActivity.java
@@ -3,9 +3,8 @@ package com.interactivemedia.backpacker.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
@@ -15,7 +14,6 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.interactivemedia.backpacker.R;
 
